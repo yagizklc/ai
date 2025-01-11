e9ec1f5dbdc7e3f94784bf3a0d4fec972e2a4682
Yağız Kılıçarslan <yagizkilicarslan@Yagzs-Mac-mini.local>
2025-01-12 02:04:42 +0300

    add tutorial code

--- app/models/cnn.py
+++ app/models/cnn.py
@@ -15,6 +15,16 @@ class BasicCNN(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.w00 = 10
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28 * 28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
+        )
 
-        self.conv1 = nn.Conv2d()
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
