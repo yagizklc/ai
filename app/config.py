24c98b0f7b32c6e0faffb5eba8bdc4e1bd9061c0
Yağız Kılıçarslan <yagizkilicarslan@Yagzs-Mac-mini.local>
2025-01-12 01:26:12 +0300

    notebook and data

--- app/config.py
+++ app/config.py
@@ -7,8 +7,15 @@ class Config(BaseSettings):
         env_prefix='__', case_sensitive=False, env_file='.env'
     )
 
-    device: str = 'mps' if torch.mps.is_available() else 'cpu'
-    hello: str
+    device: str = (
+        'cuda'
+        if torch.cuda.is_available()
+        else 'mps'
+        if torch.backends.mps.is_available()
+        else 'cpu'
+    )
+
+    batch_size: int = 64
 
 
 config = Config()
--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
 import torch
 
 from app.config import config
+from app.utils.loader import get_data
 
 
 def main():
     print(f'PyTorch version: {torch.__version__}')
     print(f'MPS (Metal) device available: {torch.backends.mps.is_available()}')
     print(f'Using device: {config.device}')
+
+    train_data, test_data = get_data()
+    for x, y in train_data:
+        print(f'Shape of X [N, C, H, W]: {x.shape}')
+        print(f'Shape of y {y.shape} {y.dtype}')
 
 
 if __name__ == '__main__':
